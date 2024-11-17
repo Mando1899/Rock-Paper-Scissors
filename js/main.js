5caa84515018a6af7387ec13d04f9bc942f6a3c2
the-mand0 <nooritimor2+new@gmail.com>
2024-11-17 19:26:00 +0300

    added simple rock-paper scissors game logic added buttons to play the game no css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const choices = document.querySelectorAll("button");
 let humanScore = 0;
 let computerScore = 0;
 
@@ -7,6 +8,45 @@ function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     return choices[randomNum];    
 }
+
+
+
+choices.forEach((button) => {
+    button.addEventListener("click", () => {
+        let humanChoice = button.innerText.toLowerCase();
+        let computerChoice = getComputerChoice();
+        console.log(humanChoice);
+        console.log(computerChoice);
+        if (humanChoice === computerChoice) {
+            alert(`Computer chose ${computerChoice}. It's a tie!`);
+        } else if (computerChoice === "rock") {
+            if (humanChoice === "scissors") {
+                computerScore++;
+                alert(`Computer chose ${computerChoice}. You lose.`)
+            } else if (humanChoice === "paper") {
+                humanScore++;
+                alert(`Computer chose ${computerChoice}. You win.`)
+            }
+        } else if (computerChoice === "paper") {
+            if (humanChoice === "rock") {
+                computerScore++;
+                alert(`Computer chose ${computerChoice}. You lose.`)
+            } else if (humanChoice === "paper") {
+                humanScore++;
+                alert(`Computer chose ${computerChoice}. You win.`)
+            }
+        } else if (computerChoice === "scissors") {
+            if (humanChoice === "paper") {
+                computerScore++;
+                alert(`Computer chose ${computerChoice}. You lose.`)
+            } else if (humanChoice === "rock") {
+                humanScore++;
+                alert(`Computer chose ${computerChoice}. You win.`)
+            }
+        }
+
+    });
+});
 
 function getHumanChoice() {
     while (true) {
@@ -19,59 +59,53 @@ function getHumanChoice() {
         }
     }
 }
+// function playRound(humanChoice, computerChoice) {
+//     console.log("Computer throws: " + computerChoice);
+//     console.log("Player throws: " + humanChoice);
 
-function playRound(humanChoice, computerChoice) {
-    console.log("Computer throws: " + computerChoice);
-    console.log("Player throws: " + humanChoice);
+//     if (humanChoice === computerChoice) {
+//         alert(`Computer chose ${computerChoice}. It's a tie!`);
+//     } else if (computerChoice === "rock") {
+//         if (humanChoice === "scissors") {
+//             computerScore++;
+//             alert(`Computer chose ${computerChoice}. You lose.`)
+//         } else if (humanChoice === "paper") {
+//             humanScore++;
+//             alert(`Computer chose ${computerChoice}. You win.`)
+//         }
+//     } else if (computerChoice === "paper") {
+//         if (humanChoice === "rock") {
+//             computerScore++;
+//             alert(`Computer chose ${computerChoice}. You lose.`)
+//         } else if (humanChoice === "paper") {
+//             humanScore++;
+//             alert(`Computer chose ${computerChoice}. You win.`)
+//         }
+//     } else if (computerChoice === "scissors") {
+//         if (humanChoice === "paper") {
+//             computerScore++;
+//             alert(`Computer chose ${computerChoice}. You lose.`)
+//         } else if (humanChoice === "rock") {
+//             humanScore++;
+//             alert(`Computer chose ${computerChoice}. You win.`)
+//         }
+//     }
 
-    if (humanChoice === computerChoice) {
-        alert(`Computer chose ${computerChoice}. It's a tie!`);
-    } else if (computerChoice === "rock") {
-        if (humanChoice === "scissors") {
-            computerScore++;
-            alert(`Computer chose ${computerChoice}. You lose.`)
-        } else if (humanChoice === "paper") {
-            humanScore++;
-            alert(`Computer chose ${computerChoice}. You win.`)
-        }
-    } else if (computerChoice === "paper") {
-        if (humanChoice === "rock") {
-            computerScore++;
-            alert(`Computer chose ${computerChoice}. You lose.`)
-        } else if (humanChoice === "paper") {
-            humanScore++;
-            alert(`Computer chose ${computerChoice}. You win.`)
-        }
-    } else if (computerChoice === "scissors") {
-        if (humanChoice === "paper") {
-            computerScore++;
-            alert(`Computer chose ${computerChoice}. You lose.`)
-        } else if (humanChoice === "rock") {
-            humanScore++;
-            alert(`Computer chose ${computerChoice}. You win.`)
-        }
-    }
+//     console.log("Player: " + humanScore + " Computer: " + computerScore);
+// }
+// function playGame() {
+//     let humanSelection = getHumanChoice();
+//     let computerSelection = getComputerChoice();
 
-    console.log("Player: " + humanScore + " Computer: " + computerScore);
-}
+//     playRound(humanSelection, computerSelection)
+    
 
-/* This game is setup to be a best of 5;
-    when ties are determined, they do not count to
-    the overall number of rounds. */
-function playGame() {
-    while (humanScore < 3 && computerScore < 3) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
+//     if (humanScore == 3) {
+//         alert("You win! 3 to 2.")
+//     } else if (computerScore == 3) {
+//         alert("Computer wins! 3 to 2.")
+//     }
+// }
 
-        playRound(humanSelection, computerSelection)
-    }
-
-    if (humanScore == 3) {
-        alert("You win! 3 to 2.")
-    } else if (computerScore == 3) {
-        alert("Computer wins! 3 to 2.")
-    }
-}
-
-console.log("Can you beat me?");
-playGame();
+// console.log("Can you beat me?");
+// playGame();
