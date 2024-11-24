15b891fce519e00b06781411174028d3077632aa
the-mand0 <nooritimor2+new@gmail.com>
2024-11-24 16:09:26 +0300

    Best of 5 the game ends when a player reaches 3 points the game is pretty much finished put it needs more style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-const choices = document.querySelectorAll("button");
+const choices = document.querySelectorAll(".choice");
 let humanScore = 0;
 let computerScore = 0;
 const humanScoreTag = document.getElementById("humanScore");
 const computerScoreTag = document.getElementById("computerScore");
 const results = document.getElementById("result");
+let roundEnd = false;
+const endGameSection = document.querySelector("#end-game");
+const playAgainBtn = document.getElementById("play-again");
+
+playAgainBtn.addEventListener("click", () =>{
+    endGameSection.style.display = "none";
+    humanScore = 0;
+    computerScore = 0;
+    results.innerText = "";
+    humanScoreTag.innerText = `Human score: ${humanScore}`;
+    computerScoreTag.innerText = `Computer score: ${computerScore}`;
+});
+
 
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
@@ -12,63 +25,66 @@ function getComputerChoice() {
     document.getElementById("computerChoice").innerText = `Computer choice: ${choices[randomNum]}`;
     return choices[randomNum];    
 }
+
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         results.innerText = `Computer chose ${computerChoice}. It's a tie!`;
     } else if (computerChoice === "rock") {
         if (humanChoice === "scissors") {
             computerScore++;
-            results.innerText = `Computer chose ${computerChoice}. You lose.`;
+            results.innerText = `Computer chose ${computerChoice}. You lose this round.`;
         } else if (humanChoice === "paper") {
             humanScore++;
-            results.innerText = `Computer chose ${computerChoice}. You win.`;
+            results.innerText = `Computer chose ${computerChoice}. You win this round.`;
         }
     } else if (computerChoice === "paper") {
         if (humanChoice === "rock") {
             computerScore++;
-            results.innerText = `Computer chose ${computerChoice}. You lose.`;
+            results.innerText = `Computer chose ${computerChoice}. You lose this round.`;
         } else if (humanChoice === "scissors") {
             humanScore++;
-            results.innerText = `Computer chose ${computerChoice}. You win.`;
+            results.innerText = `Computer chose ${computerChoice}. You win this round.`;
         }
     } else if (computerChoice === "scissors") {
         if (humanChoice === "paper") {
             computerScore++;
-            results.innerText = `Computer chose ${computerChoice}. You lose.`;
+            results.innerText = `Computer chose ${computerChoice}. You lose this round.`;
         } else if (humanChoice === "rock") {
             humanScore++;
-            results.innerText = `Computer chose ${computerChoice}. You win.`;
+            results.innerText = `Computer chose ${computerChoice}. You win this round.`;
         }
     }
     humanScoreTag.innerText = `Human score: ${humanScore}`;
-    computerScoreTag.innerText = `Computer score: ${computerScore}`
+    computerScoreTag.innerText = `Computer score: ${computerScore}`;
 }
 
+function endGame() {
+    endGameSection.style.display = "inline";
+    const endGameText = endGameSection.firstElementChild;
+    if (humanScore === 3) {
+        endGameText.innerText = "You won! Congratulations.";
+    } else if (computerScore === 3) {
+        endGameText.innerText = "You lose! Better luck next time.";
+    }
+    roundEnd = true;
+}
+
+function playGame(humanChoice, computerChoice) {
+    playRound(humanChoice, computerChoice);
+    if (humanScore === 3 || computerScore === 3) {
+        endGame();
+    }
+}
 
 choices.forEach((button) => {
     button.addEventListener("click", () => {
-        let humanChoice = button.innerText.toLowerCase();
-        document.getElementById("humanChoice").innerText = `Human Choice: ${humanChoice}`
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-
+        if (!roundEnd) {
+            let humanChoice = button.innerText.toLowerCase();
+            document.getElementById("humanChoice").innerText = `Human choice: ${humanChoice}`
+            let computerChoice = getComputerChoice();
+            playGame(humanChoice, computerChoice);
+        }
     });
 });
 
 
-// function playGame() {
-//     let humanSelection = getHumanChoice();
-//     let computerSelection = getComputerChoice();
-
-//     playRound(humanSelection, computerSelection)
-    
-
-//     if (humanScore == 3) {
-//         alert("You win! 3 to 2.")
-//     } else if (computerScore == 3) {
-//         alert("Computer wins! 3 to 2.")
-//     }
-// }
-
-// console.log("Can you beat me?");
-// playGame();
